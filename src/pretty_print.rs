4b1641b2d3b68950c104e48237dfb3d65973d92b
huangshijia <huangshijia@kylinos.cn>
2025-11-10 17:08:09 +0800

    license: set as the apache license 2.0

--- src/pretty_print.rs
+++ src/pretty_print.rs
@@ -1,6 +1,7 @@
-// This Source Code Form is subject to the terms of the Mozilla Public License,
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can
-// obtain one at https://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 WeiKang Guo <guoweikang.kernel@gmail.com
+// Copyright (C) 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSE for license details.
 
 pub fn print_node(
     f: &mut core::fmt::Formatter<'_>,
